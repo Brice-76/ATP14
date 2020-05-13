37a9505007149320f34a44663e9557d2e55a5d23
brice <brice.chauvat@isen-ouest.yncrea.fr>
2020-05-13 16:09:27 +0200

    COMMIT

---
+++ Exercice.py
@@ -0,0 +1,19 @@
+from PySide2 import QtWidgets
+
+
+class Window(QtWidgets.QWidget) :
+    def __init__(self):
+        a=QtWidgets.QLayout()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   app = Window([])
+   win = Window()
+   win.show()
+   app.exec_()
