86db37453ea005dcec15de81ff2b43695220c2d3
brice <brice.chauvat@isen-ouest.yncrea.fr>
2020-05-13 17:06:34 +0200

    COMMIT

--- Exercice 3.py
+++ Exercice 3.py
@@ -1,4 +1,6 @@
-from PySide2.QtWidgets import QWidget,QVBoxLayout,QApplication,QBoxLayout, QGridLayout,QLabel, QPushButton,QTextEdit,QHBoxLayout
+from PySide2.QtWidgets import QWidget,QVBoxLayout,QApplication,QBoxLayout, QGridLayout,QLabel, QPushButton,QTextEdit,QHBoxLayout,QProgressBar,QLineEdit
+from PySide2 import QtGui
+from PySide2 import QtCore
 
 
 class Window(QWidget) :
@@ -6,4 +8,29 @@ class Window(QWidget) :
         QWidget.__init__(self)
         self.setFixedSize(400,300)
         self.setWindowTitle('IMH')
+        icon=QtGui.QIcon('../fr-flag.png')
+        self.setWindowIcon(icon)
+        label=QLabel('Hello World')
+        label.setAlignment(QtCore.Qt.AlignCenter)
 
+        layout=QVBoxLayout()
+        barre_chargement=QProgressBar()
+        barre_chargement.setValue(30)
+        line=QLineEdit()
+        button=QPushButton('push_me')
+        label.setToolTip(('SALUT'))
+        layout.addWidget(label)
+        layout.addWidget(barre_chargement)
+        layout.addWidget(line)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+
+
+
+
+if __name__ == "__main__":
+   app = QApplication([])
+   win = Window()
+   win.show()
+   app.exec_()
