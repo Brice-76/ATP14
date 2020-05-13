410a35cb9609461c8c3624abb04e09743a1dccaa
brice <brice.chauvat@isen-ouest.yncrea.fr>
2020-05-13 16:42:13 +0200

    COMMIT

--- Exercice 2.py
+++ Exercice 2.py
@@ -1,17 +1,27 @@
-from PySide2.QtWidgets import QWidget,QVBoxLayout,QApplication,QBoxLayout, QGridLayout,QLabel, QPushButton,QTextEdit
+from PySide2.QtWidgets import QWidget,QVBoxLayout,QApplication,QBoxLayout, QGridLayout,QLabel, QPushButton,QTextEdit,QHBoxLayout
 
 class Window(QWidget) :
     def __init__(self):
-        super(Window, self).__init__()
+        Window.__init__(self)
         self.setWindowTitle('IHM')
         layout=QGridLayout()
+
         label=QLabel('Laisser un commentaire')
         button_1=QPushButton('Succes')
         button_2=QPushButton('Cancel')
         textEdit=QTextEdit()
-        layout.addWidget(label)
-        layout.addWidget(textEdit)
-        layout.addWidget(button_1)
-        layout.addWidget(button_2)
+        layout.addWidget(label,0,0,1,0,allignement=0)
+        layout.addWidget(textEdit,1,1)
+        layout.addWidget(button_1,3,0)
+        layout.addWidget(button_2,3,1)
 
 
+        self.setLayout(layout)
+
+
+
+if __name__ == "__main__":
+   app = QApplication([])
+   win = Window()
+   win.show()
+   app.exec_()
--- Exercice.py
+++ Exercice.py
@@ -3,7 +3,7 @@ from PySide2.QtWidgets import QWidget,QVBoxLayout,QApplication,QBoxLayout
 
 class Window(QWidget) :
     def __init__(self):
-        super(Window, self).__init__()
+        Window.__init__(self)
         layout=QVBoxLayout()
         for i in range (0,10) :
             k=QWidget()
